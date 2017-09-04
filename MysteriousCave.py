db01cc77d3c2d13870ddf523f8f46acd9bbf850f
Joseph G Evans <29513395+JosephGEvans@users.noreply.github.com>
2017-09-04 12:37:24 -0400

    Added common variables to every room function, and fixed some text formatting.

--- MysteriousCave.py
+++ MysteriousCave.py
@@ -30,14 +30,14 @@ def mcdu():
     "Catch all for when your words don't apply to the specific situation."
 
 
-    elif mcinput.lower() == "get":
+    if mcinput.lower() == "get":
         print("Get?  Get what?  You got nothing.")
     elif mcinput.lower().startswith("get "):
         print(f"""
         \rYou consider getting {mcinput[4:]} in case you might need it later.
         \rJust between us, {player}, you're not gonna.  You should leave
         \r{mcinput[4:]} behind.""")
-    if "back" in mcinput.lower():
+    elif "back" in mcinput.lower():
         exec(last_room + "(this_room)")
         return
     elif mcp("left") or mcp("right") or mcp("straight") or mcp("forward"):
@@ -49,12 +49,14 @@ def mcdu():
         """)
     elif "help" in mcinput.lower():
         print("""Here are some things you can try:
+
         LOOK at something
         GET something
         USE something
         Check out your INVENTORY (aka INV to the "in crowd" who use cool words)
         TALK to someone?  Are there even people here?
-        You can always try to go EAST, WEST, NORTH, or SOUTH.""")
+        You can always try to go EAST, WEST, NORTH, or SOUTH.
+        """)
         exec(this_room + "(last_room)")
         return
     else:
@@ -69,17 +71,15 @@ def start():
     global player
     player = input("> ")
     os.system('cls')
-    print(f"""
-    Well, hello there, {player}.
-    Tell me, {player}, how does this story sound:
+    print(
+    f"""\rWell, hello there, {player}. Tell me, how does this story sound?
 
     {player} is looking at a mysterious cave...
     A snake is slithering towards {player}!
     Oh! But then, the snake keeps on going.
     {player} is very relieved.
 
-    If your name looks weird there, you can change it.
-    """)
+    \rIf your name looks weird there, you can change it.""")
 
     while True:
         print("Would you like to change your name?")
@@ -94,7 +94,7 @@ def start():
             cave_entrance("cave_entrance")
             break
         elif mcp("no"):
-            print("Great!  Let's get started.")
+            print("Great.  Let's get started!")
             cave_entrance("cave_entrance")
             break
         else:
@@ -106,10 +106,10 @@ def cave_entrance(previous):
     global last_room, this_room, player
     last_room = previous
     this_room = "cave_entrance"
-    print(f"""
-    \rIt looks like you've found yourself in the entrance to a cave!  The light
-    \rfrom outside shows paths leading EAST, WEST, and NORTH.  Which way would
-    \ryou like to go?""")
+    print(
+    f"""\rIt looks like you've found yourself in the entrance to a cave!  The
+    \rlight from outside shows paths leading EAST, WEST, and NORTH.  Which way
+    \rwould you like to go?""")
     mci()
     # offer choices left, right, forwared, back out of the cave
     if mcp("east"):
@@ -134,11 +134,11 @@ def hobo_bedroom(previous):
     this_room = "hobo_bedroom"
     last_room = previous
     global hobo_bedroom_rocks_moved
-    print(f"""
-    You look around with your handy flashlight, and see some trash. Someone
-    might live here.  One cave wall looks like it was arranged by human hands.
-    You can EXAMINE the wall, or exit back to the WEST.
-    """)
+    print(
+    f"""\rYou look around with your handy flashlight, and see some trash.
+    \rSomeone might live here.  One cave wall looks like it was arranged by
+    \rhuman hands.  You can EXAMINE the wall, or exit back to the WEST."""
+    )
 
     while True:
         mci()
@@ -162,54 +162,84 @@ def hobo_bedroom(previous):
             mcdu()
 
 
-def hobo_cache():
+def hobo_cache(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "hobo_cache"
     pass
 
 
-def perfect_cube_room():
+def perfect_cube_room(previous):
     #secret entrance to finished path
     #cant find secret entrance unless exited through it once (no seams in room)
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "perfect_cube_room"
     pass
 
 
-def long_walk():
+def long_walk(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "long_walk"
     pass
 
 
-def empty_cavern():
+def empty_cavern(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "empty_cavern"
     pass
 
 
-def longer_twisty_walk():
+def longer_twisty_walk(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "longer_twisty_walk"
     #climb up
     #climb down
     pass
 
 
-def vast_expanse():
+def vast_expanse(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "vast_expanse"
     pass
 
 
-def narrow_squeeze():
+def narrow_squeeze(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "narrow_squeeze"
     #forward
     #back
     pass
 
 
-def open_cavern():
+def open_cavern(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "open_cavern"
     #pool
     #door?
     pass
 
 
-def transition_hall():
+def transition_hall(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "transition_hall"
     #leads to finished room
     #door requiring a key (found back in the cave somewhere...)
     #back
     pass
 
 
-def finished_room():
+def finished_room(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "finished_room"
     #back to transition hall
     #left door to bathroom
     #straight ahead to bedroom
@@ -217,7 +247,10 @@ def finished_room():
     pass
 
 
-def bath_room():
+def bath_room(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "bath_room"
     #toilet
     #shower
     #med cabinet
@@ -225,43 +258,64 @@ def bath_room():
     pass
 
 
-def bed_room():
+def bed_room(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "bed_room"
     #finished room
     pass
 
 
-def finished_hallway():
+def finished_hallway(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "finished_hallway"
     #kitchen
     #study
     #finished_room
     pass
 
 
-def kitchen():
+def kitchen(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "kitchen"
     #back_porch
     #hallway
     pass
 
 
-def back_porch():
+def back_porch(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "back_porch"
     # overlook pool in open cavern
     pass
 
 
-def study():
+def study(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "study"
     #library
     #desk
     #secret entrance
     pass
 
 
-def secret_room():
+def secret_room(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "secret_room"
     #confrontation?
     #exit to finished path
     pass
 
 
-def finished_path():
+def finished_path(previous):
+    global last_room, this_room, player
+    last_room = previous
+    this_room = "finished_path"
     #leads back to cube room
     pass
 
